d35852a292018924e33caf83ba441c25dbc419bd
mariom <mariom9267idb@gmail.com>
2022-08-26 01:39:11 +0600

    templete setup

---
+++ resources/views/backend/layouts/inc/script.blade.php
@@ -0,0 +1,26 @@
+ <!-- Vendor Scripts Start -->
+    <script src="{{ asset('assets/backend') }}/js/vendor/jquery-3.5.1.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/vendor/OverlayScrollbars.min.js"></script>
+    {{-- <script src="{{ asset('assets/backend') }}/js/vendor/autoComplete.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/vendor/clamp.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/vendor/Chart.bundle.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/vendor/chartjs-plugin-rounded-bar.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/vendor/jquery.barrating.min.js"></script> --}}
+    <!-- Vendor Scripts End -->
+
+    <!-- Template Base Scripts Start -->
+    <script src="{{ asset('assets/backend') }}/font/CS-Line/csicons.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/base/helpers.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/base/globals.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/base/nav.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/base/search.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/base/settings.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/base/init.js"></script>
+    <!-- Template Base Scripts End -->
+    <!-- Page Specific Scripts Start -->
+    {{-- <script src="{{ asset('assets/backend') }}/js/cs/charts.extend.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/pages/dashboard.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/common.js"></script>
+    <script src="{{ asset('assets/backend') }}/js/scripts.js"></script> --}}
+    <!-- Page Specific Scripts End -->
---
+++ resources/views/backend/pages/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('backend.layouts.master')
+@section('title')
+Dashboard
+@endsection
+@section('admin_content')
+<div class="row">
+    <h1>Start from Here</h1>
+</div>
+@endsection
