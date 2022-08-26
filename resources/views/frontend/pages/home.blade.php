416648eeb39e43e2beeea8f8cb9a2eab52e445c4
mariom <mariom9267idb@gmail.com>
2022-08-26 23:11:18 +0600

    frontend page Setup

---
+++ resources/views/frontend/pages/home.blade.php
@@ -0,0 +1,6 @@
+ @include('frontend.pages.widgets.slider')
+ @include('frontend.pages.widgets.featured')
+ @include('frontend.pages.widgets.countdown')
+ @include('frontend.pages.widgets.best-seller')
+ @include('frontend.pages.widgets.latest-product')
+ @include('frontend.pages.widgets.testimonial')
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('backend.pages.dashboard');
 });
+Route::get('/', function () {
+    return view('frontend.layouts.master');
+});
